94dccc722f1361ec0a88262e218cc0f2e5bb99ef
Martin Listiak <martin.listiak@praxuj.sk>
2021-10-05 12:35:29 +0200

    feat: Proxy exchange rates request

--- src/api/exchange-rates/exchange-rates.ts
+++ src/api/exchange-rates/exchange-rates.ts
@@ -1,10 +1,8 @@
 import { ExchangeRatesResponse } from '@src/types'
 import { transformExchangeRates } from './exchange-rates.transform'
 
-const exchangeRatesApiUrl = 'http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt'
-
 export async function fetchExchangeRates(): Promise<ExchangeRatesResponse> {
-	const response = await fetch(exchangeRatesApiUrl)
+	const response = await fetch('/api/exchange-rates')
 
 	return transformExchangeRates(await response.json())
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const exchangeRatesApiUrl = 'http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt'
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	resolve: {
@@ -10,4 +12,13 @@ export default defineConfig({
 		},
 	},
 	plugins: [react()],
+	server: {
+		proxy: {
+			'/api/exchange-rates': {
+				target: exchangeRatesApiUrl,
+				changeOrigin: true,
+				rewrite: path => path.replace(/^\/api\/exchange-rates/, ''),
+			},
+		},
+	},
 })
